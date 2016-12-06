6d05bfcd12060a9705ba70b6317518ef8c7245c8
Michael Leditschke <mike@ammd.com.au>
2016-12-06 17:09:00 +1100

    setContext.sh must always be run from AUTOMATION_BASE_DIR

--- jenkins/aws/setContext.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ -n "${AUTOMATION_DEBUG}" ]]; then set ${AUTOMATION_DEBUG}; fi
-trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
-
-${AUTOMATION_BASE_DIR}/setContext.sh "$@"
-RESULT=$?

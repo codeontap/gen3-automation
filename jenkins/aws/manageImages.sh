b5fbe65d48fe0825f648d54bbdd2a4565e6c26a7
Michael Leditschke <ml019@ammd.com.au>
2016-12-21 16:14:46 +1100

    Use IMAGE_FORMAT to control required packaging

--- jenkins/aws/manageImages.sh
+++ jenkins/aws/manageImages.sh
@@ -8,14 +8,26 @@ if [[ -n "${AUTOMATION_DEBUG}" ]]; then set ${AUTOMATION_DEBUG}; fi
 
 SLICE_ARRAY=(${SLICE_LIST})
 CODE_COMMIT_ARRAY=(${CODE_COMMIT_LIST})
+IMAGE_FORMAT_ARRAY=(${IMAGE_FORMAT_LIST})
 
-# Package for docker if required
-if [[ -f Dockerfile ]]; then
-    ${AUTOMATION_DIR}/manageDocker.sh -b -s "${SLICE_ARRAY[0]}" -g "${CODE_COMMIT_ARRAY[0]}"
-    RESULT=$?
-    if [[ "${RESULT}" -ne 0 ]]; then
+case ${IMAGE_FORMAT_ARRAY[0]} in
+    docker)
+        # Package for docker
+        if [[ -f Dockerfile ]]; then
+            ${AUTOMATION_DIR}/manageDocker.sh -b -s "${SLICE_ARRAY[0]}" -g "${CODE_COMMIT_ARRAY[0]}"
+            RESULT=$?
+            if [[ "${RESULT}" -ne 0 ]]; then
+                exit
+            fi
+        else
+            echo -e "\nDockerfile missing"
+            exit
+        fi
+        ;;
+
+    # TODO: Perform checks for AWS Lambda packaging - not sure yet what to check for as a marker
+    *)
+        echo -e "\nUnsupported image format \"${IMAGE_FORMAT}\""
         exit
-    fi
-fi
-
-# TODO: Package for AWS Lambda if required - not sure yet what to check for as a marker
+        ;;
+esac

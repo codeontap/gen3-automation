8f36ccfa39142a1ee26bbe0aa2fe4698cb631370
Michael Leditschke <mike@ammd.com.au>
2017-02-03 19:45:47 +1100

    Fix path to appsettings data when accepting a release

--- jenkins/aws/acceptReleaseSetup.sh
+++ jenkins/aws/acceptReleaseSetup.sh
@@ -5,7 +5,7 @@ trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
 
 # Get all the slice commit information
 ${AUTOMATION_DIR}/manageBuildReferences.sh -f \
-    -g ${AUTOMATION_DATA_DIR}/${ACCOUNT}/config/${PRODUCT}/appsettings/${FROM_SEGMENT}
+    -g ${AUTOMATION_DATA_DIR}/${ACCOUNT}/config/${PRODUCT}/appsettings/${SEGMENT}
 RESULT=$?
 
 

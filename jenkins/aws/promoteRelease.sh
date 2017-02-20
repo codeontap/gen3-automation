b9e97b5181e7557cc23515099018bda7e14b07b0
Michael Leditschke <mike@ammd.com.au>
2017-02-20 22:43:14 +1100

    Always pull in lower segment config repo when promoting a release

--- jenkins/aws/promoteRelease.sh
+++ jenkins/aws/promoteRelease.sh
@@ -13,19 +13,16 @@ fi
 # Verify the build information
 if [[ "${RELEASE_MODE}" == "${RELEASE_MODE_PROMOTION}" ]]; then
 
-    # Get the from settings
-    FROM_SETTINGS_DIR=${AUTOMATION_DATA_DIR}/${FROM_ACCOUNT}/config/${PRODUCT}/appsettings/${FROM_SEGMENT}
-    if [[ ! -d ${FROM_SETTINGS_DIR} ]]; then
-        # Settings not already there as a result of the segments sharing an account
-        FROM_PRODUCT_DIR=${AUTOMATION_DATA_DIR}/${FROM_ACCOUNT}/from_config/${PRODUCT}
-        mkdir -p ${FROM_PRODUCT_DIR}
-        ${AUTOMATION_DIR}/manageRepo.sh -c -l "from product config" \
-            -n "${FROM_PRODUCT_CONFIG_REPO}" -v "${FROM_PRODUCT_GIT_PROVIDER}" \
-            -d "${FROM_PRODUCT_DIR}" -b "${ACCEPTANCE_TAG}"
-        RESULT=$?
-        if [[ "${RESULT}" -ne 0 ]]; then exit; fi
-        FROM_SETTINGS_DIR=${FROM_PRODUCT_DIR}/appsettings/${FROM_SEGMENT}
-    fi
+    # Get the from settings in a separate dir - name it differently
+    # in case both segments in the same repo
+    FROM_PRODUCT_DIR=${AUTOMATION_DATA_DIR}/${FROM_ACCOUNT}/from_config/${PRODUCT}
+    mkdir -p ${FROM_PRODUCT_DIR}
+    ${AUTOMATION_DIR}/manageRepo.sh -c -l "from product config" \
+        -n "${FROM_PRODUCT_CONFIG_REPO}" -v "${FROM_PRODUCT_GIT_PROVIDER}" \
+        -d "${FROM_PRODUCT_DIR}" -b "${ACCEPTANCE_TAG}"
+    RESULT=$?
+    if [[ "${RESULT}" -ne 0 ]]; then exit; fi
+    FROM_SETTINGS_DIR=${FROM_PRODUCT_DIR}/appsettings/${FROM_SEGMENT}
             
     # Pull in the current build references in lower segment
     ${AUTOMATION_DIR}/manageBuildReferences.sh -f \

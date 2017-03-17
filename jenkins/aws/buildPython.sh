835f9ab2f6ac8db63b25485447c5ff168e88d8ab
Michael Leditschke <mike@ammd.com.au>
2017-03-17 13:03:17 +1100

    Initial support for python based builds

---
+++ jenkins/aws/buildPython.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+if [[ -n "${AUTOMATION_DEBUG}" ]]; then set ${AUTOMATION_DEBUG}; fi
+trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
+
+# Is this really a python based project
+if [[ ! -f requirements.txt ]]; then
+   echo -e "\nNo requirements.txt - is this really a python base repo?" >&2
+   RESULT=1
+   exit
+fi
+
+# Set up the virtual build environment
+virtualenv .venv
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+   echo -e "\nCreation of virtual build environment failed" >&2
+   exit
+fi
+. .venv/bin/activate
+
+# Process requirements files
+shopt -s nullglob
+REQUIREMENTS_FILES=( requirements*.txt )
+for REQUIREMENTS_FILE in "${REQUIREMENTS_FILES[@]}"; do
+    pip install -r ${REQUIREMENTS_FILE} --upgrade
+    RESULT=$?
+    if [ $RESULT -ne 0 ]; then
+       echo -e "\nInstallation of requirements failed" >&2
+       exit
+    fi
+done
+
+if [[ -f package.json ]]; then
+    npm install --unsafe-perm
+    RESULT=$?
+    if [ $RESULT -ne 0 ]; then
+       echo -e "\nnpm install failed" >&2
+       exit
+    fi
+fi
+
+# Run bower as part of the build if required
+if [[ -f bower.json ]]; then
+    bower install --allow-root
+    RESULT=$?
+    if [ $RESULT -ne 0 ]; then
+       echo -e "\nbower install failed" >&2
+       exit
+    fi
+fi
+
+# Package for lambda if required
+if [[ -f zappa_settings.json ]]; then
+    BUILD=`zappa package dev 2> /dev/null | tail -1 | cut -d' ' -f3`
+    if [[ -f ${BUILD} ]]; then
+        mkdir -p dist/
+        mv $BUILD dist/lambda.zip
+    fi
+fi
+
+# Clean up
+npm prune --production
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+   echo -e "\nnpm prune failed" >&2
+   exit
+fi
+
+. ${AUTOMATION_DIR}/manageImages.sh
+RESULT=$?

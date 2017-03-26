669bf86fe11dcf11159868dd83ff5dc7bf2b4ceb
Michael Leditschke <mike@ammd.com.au>
2017-03-27 01:09:09 +1100

    For now, list build references as part of acceptRelease

--- jenkins/aws/acceptRelease.sh
+++ jenkins/aws/acceptRelease.sh
@@ -3,6 +3,11 @@
 if [[ -n "${AUTOMATION_DEBUG}" ]]; then set ${AUTOMATION_DEBUG}; fi
 trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
 
+# Include the build information in the detail message
+${AUTOMATION_DIR}/manageBuildReferences.sh -l
+RESULT=$?
+if [[ "${RESULT}" -ne 0 ]]; then exit; fi
+
 # Tag the builds
 ${AUTOMATION_DIR}/manageBuildReferences.sh -a "${RELEASE_TAG}"
 RESULT=$?

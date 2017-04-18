4a6c061f529f664e624bb60fdf7146e598bad581
Michael Leditschke <mike@ammd.com.au>
2017-04-18 15:03:47 +1000

    Add generic Javascript build

---
+++ jenkins/aws/buildJS.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+if [[ -n "${AUTOMATION_DEBUG}" ]]; then set ${AUTOMATION_DEBUG}; fi
+trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
+
+# Select the package manage to use
+if [[ -z "${NODE_PACKAGE_MANAGER}" ]]; then
+    if $(which yarn > /dev/null 2>&1) ; then
+        NODE_PACKAGE_MANAGER="yarn"
+    else
+        NODE_PACKAGE_MANAGER="npm"
+    fi
+fi
+
+# Make sure we are in the build directory
+cd ${AUTOMATION_BUILD_DIR}
+
+${NODE_PACKAGE_MANAGER} install
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+   echo -e "\nnpm install failed" >&2
+   exit
+fi
+
+# Run bower as part of the build if required
+if [[ -f bower.json ]]; then
+    bower install --allow-root
+    RESULT=$?
+    if [ $RESULT -ne 0 ]; then
+       echo -e "\nbower install failed" >&2
+       exit
+    fi
+fi
+
+# Determine required tasks
+# Build is always first
+REQUIRED_TASKS=( "build" )
+
+# Perform format specific tasks if defined
+IMAGE_FORMATS_ARRAY=(${IMAGE_FORMATS_LIST})
+IFS="," read -ra FORMATS <<< "${IMAGE_FORMATS_ARRAY[0]}"
+REQUIRED_TASKS=( "${REQUIRED_TASKS[@]}" "${FORMATS[@]}" )
+
+# The build file existence checks below rely on nullglob
+# to return nothing if no match
+shopt -s nullglob
+BUILD_FILES=(?runtfile.js ?ulpfile.js package.json)
+
+# Perform build tasks in the order specified
+for REQUIRED_TASK in "${REQUIRED_TASKS[@]}"; do
+    for BUILD_FILE in "${BUILD_FILES[@]}"; do
+        BUILD_TASKS=()
+        case ${BUILD_FILE} in
+            ?runtfile.js)
+                BUILD_TASKS=( $(grunt -h --no-color | sed -n '/^Available tasks/,/^$/ {s/^  *\([^ ]\+\)  [^ ]\+.*$/\1/p}') )
+                BUILD_UTILITY="grunt"
+                ;;
+
+            ?ulpfile.js)
+                BUILD_TASKS=( $(gulp --tasks-simple) )
+                BUILD_UTILITY="gulp"
+                ;;
+
+            package.json)
+                BUILD_TASKS=( jq -r '.scripts | select(.!=null) | keys[]' < package.json )
+                BUILD_UTILITY="${NODE_PACKAGE_MANAGER} run"
+                ;;
+        esac
+
+        if [[ "${BUILD_TASKS[*]/${REQUIRED_TASK}/XXfoundXX}" != "${BUILD_TASKS[*]}" ]]; then
+            ${BUILD_UTILITY} ${REQUIRED_TASK}
+            RESULT=$?
+            if [ $RESULT -ne 0 ]; then
+                echo -e "\n${BUILD_UTILITY} \"${TASK}\" task failed" >&2
+                exit
+            fi
+
+            # Task complete so stop looking for build file supporting it
+            break
+        fi
+    done
+done
+
+# Clean up
+if [[ "${NODE_PACKAGE_MANAGER}" == "npm" ]]; then
+    ${NODE_PACKAGE_MANAGER} prune --production
+    RESULT=$?
+    if [ $RESULT -ne 0 ]; then
+       echo -e "\nnpm prune failed" >&2
+       exit
+    fi
+fi
+
+# All good
+RESULT=0

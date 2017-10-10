37a082538658a0f3f909db39fd2b33bfb03125bc
Michael Leditschke <mike@ammd.com.au>
2017-10-11 09:20:18 +1100

    Rework swagger generation
    Add a function to make multi-location file searching simpler
    
    Support the swagger specification being in a separate top level repo
    directory to permit sharing of it across live and mock versions of apis.

--- jenkins/aws/buildSwagger.sh
+++ jenkins/aws/buildSwagger.sh
@@ -12,24 +12,29 @@ mkdir -p ${DIST_DIR}
 SWAGGER_RESULT_FILE="${DIST_DIR}/swagger.zip"
 
 # Possible input files
-SWAGGER_SPEC_FILE="${AUTOMATION_BUILD_DIR}/swagger.json"
-SWAGGER_SPEC_YAML_FILE="${AUTOMATION_BUILD_DIR}/swagger.yaml"
-SWAGGER_SPEC_YAML_EXTENSIONS_FILE="${AUTOMATION_BUILD_DIR}/swagger_extensions.yaml"
-if [[ -f "${AUTOMATION_BUILD_DEVOPS_DIR}/codeontap/swagger_extensions.yaml" ]]; then
-    SWAGGER_SPEC_YAML_EXTENSIONS_FILE="${AUTOMATION_BUILD_DEVOPS_DIR}/codeontap/swagger_extensions.yaml"
-fi
+SWAGGER_SPEC_FILE=$(findFile \
+                    "${AUTOMATION_BUILD_DIR}/swagger.json" \
+                    "${AUTOMATION_BUILD_DIR}/../*spec/swagger.json")
+SWAGGER_SPEC_YAML_FILE=$(findFile \
+                    "${AUTOMATION_BUILD_DIR}/swagger.yaml" \
+                    "${AUTOMATION_BUILD_DIR}/../*spec/swagger.yaml")
+SWAGGER_SPEC_YAML_EXTENSIONS_FILE=$(findFile \
+                    "${AUTOMATION_BUILD_DIR}/swagger_extensions.yaml"
+                    "${AUTOMATION_BUILD_DEVOPS_DIR}/codeontap/swagger_extensions.yaml")
 
 # Convert yaml files to json, possibly including a separate yaml based extensions file
 if [[ -f "${SWAGGER_SPEC_YAML_FILE}" ]]; then
     if [[ -f "${SWAGGER_SPEC_YAML_EXTENSIONS_FILE}" ]]; then
         # Combine the two
-        cp "${SWAGGER_SPEC_YAML_EXTENSIONS_FILE}" "${AUTOMATION_BUILD_DIR}/temp_swagger_extensions.yaml"
+        cp "${SWAGGER_SPEC_YAML_FILE}" "${AUTOMATION_BUILD_DIR}/temp_swagger_copy.yaml"
+        cp "${SWAGGER_SPEC_YAML_EXTENSIONS_FILE}" "${AUTOMATION_BUILD_DIR}/temp_swagger_extensions_copy.yaml"
         docker run --rm \
             -v ${AUTOMATION_BUILD_DIR}:/app/indir -v ${AUTOMATION_BUILD_DIR}:/app/outdir \
             codeontap/utilities sme merge \
-            /app/indir/swagger.yaml \
-            /app/indir/temp_swagger_extensions.yaml \
+            /app/indir/temp_swagger_copy.yaml \
+            /app/indir/temp_swagger_extensions_copy.yaml \
             /app/outdir/temp_swagger.yaml
+
         # Use the combined file
         SWAGGER_SPEC_YAML_FILE="${AUTOMATION_BUILD_DIR}/temp_swagger.yaml"       
     fi
@@ -47,20 +52,20 @@ fi
 # Validate it
 # We use a few different validators until we settle on a preferred one
 VALIDATORS=( \
-"swagger       validate /app/indir/${SWAGGER_SPEC_FILE##*/}" \
-"swagger-tools validate /app/indir/${SWAGGER_SPEC_FILE##*/}" \
-"ajv           validate -d /app/indir/${SWAGGER_SPEC_FILE##*/} -s /usr/local/lib/node_modules/swagger-schema-official/schema.json")
+"swagger       validate /app/indir/$(fileName \"${SWAGGER_SPEC_FILE}\" )" \
+"swagger-tools validate /app/indir/$(fileName \"${SWAGGER_SPEC_FILE}\" )" \
+"ajv           validate -d /app/indir/$(fileName \"${SWAGGER_SPEC_FILE}\" )-s /usr/local/lib/node_modules/swagger-schema-official/schema.json")
 for VALIDATOR in "${VALIDATORS[@]}"; do
-    docker run --rm -v ${SWAGGER_SPEC_FILE%/*}:/app/indir codeontap/utilities ${VALIDATOR}
+    docker run --rm -v $(filePath "${SWAGGER_SPEC_FILE}"):/app/indir codeontap/utilities ${VALIDATOR}
     RESULT=$?
     [[ "${RESULT}" -ne 0 ]] && fatal "Swagger file is not valid"
 done
 
 # Augment the swagger file if required
-APIGW_CONFIG="${AUTOMATION_BUILD_DIR}/apigw.json"
-if [[ -f "${AUTOMATION_BUILD_DEVOPS_DIR}/codeontap/apigw.json" ]]; then
-    APIGW_CONFIG="${AUTOMATION_BUILD_DEVOPS_DIR}/codeontap/apigw.json"
-fi
+APIGW_CONFIG=$(findFile \
+                "${AUTOMATION_BUILD_DIR}/apigw.json" \
+                "${AUTOMATION_BUILD_DEVOPS_DIR}/codeontap/apigw.json")
+
 if [[ -f "${APIGW_CONFIG}" ]]; then
 
     # Generate the swagger file
@@ -78,9 +83,9 @@ fi
 
 # Generate documentation
 docker run --rm \
-    -v ${SWAGGER_SPEC_FILE%/*}:/app/indir -v ${DIST_DIR}:/app/outdir \
+    -v $(filePath "${SWAGGER_SPEC_FILE}"):/app/indir -v ${DIST_DIR}:/app/outdir \
     codeontap/utilities swagger2aglio \
-     --input=/app/indir/${SWAGGER_SPEC_FILE##*/} --output=/app/outdir/apidoc.html  \
+     --input=/app/indir/$(fileName "${SWAGGER_SPEC_FILE}") --output=/app/outdir/apidoc.html  \
      --theme-variables slate --theme-template triple
 RESULT=$?
 [[ "${RESULT}" -ne 0 ]] && fatal "Swagger file documentation generation failed"

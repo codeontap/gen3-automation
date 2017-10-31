3517752925f6ddb8250c30976e7e9096d9b75a55
Michael Leditschke <mike@ammd.com.au>
2017-11-01 01:04:39 +1100

    Script tidy-up

--- jenkins/aws/acceptReleaseSetup.sh
+++ jenkins/aws/acceptReleaseSetup.sh
@@ -2,6 +2,7 @@
 
 [[ -n "${AUTOMATION_DEBUG}" ]] && set ${AUTOMATION_DEBUG}
 trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
+. "${AUTOMATION_BASE_DIR}/common.sh"
 
 # Get all the deployment unit commit information
 ${AUTOMATION_DIR}/manageBuildReferences.sh -f

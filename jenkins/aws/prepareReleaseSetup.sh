18c6107a83633db6c441405a57feeef8d9242feb
Michael Leditschke <mike@ammd.com.au>
2017-11-01 01:04:39 +1100

    Script tidy-up

--- jenkins/aws/prepareReleaseSetup.sh
+++ jenkins/aws/prepareReleaseSetup.sh
@@ -2,6 +2,7 @@
 
 [[ -n "${AUTOMATION_DEBUG}" ]] && set ${AUTOMATION_DEBUG}
 trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
+. "${AUTOMATION_BASE_DIR}/common.sh"
 
 # Add release number to details
 DETAIL_MESSAGE="release=${RELEASE_TAG}, ${DETAIL_MESSAGE}"

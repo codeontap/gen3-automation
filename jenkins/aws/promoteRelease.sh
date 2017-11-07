28c1301e74898f5e5b4eca75638b6bddbb324665
Michael Leditschke <mike@ammd.com.au>
2017-11-07 16:45:26 +1100

    Support composite repos in promotion process

--- jenkins/aws/promoteRelease.sh
+++ jenkins/aws/promoteRelease.sh
@@ -15,14 +15,24 @@ if [[ "${RELEASE_MODE}" == "${RELEASE_MODE_PROMOTION}" ]]; then
 
     # Get the from settings in a separate dir - name it differently
     # in case both segments in the same repo
-    FROM_PRODUCT_DIR=${AUTOMATION_DATA_DIR}/${FROM_ACCOUNT}/from_config/${PRODUCT}
-    mkdir -p ${FROM_PRODUCT_DIR}
+    FROM_PRODUCT_BASE_DIR="${AUTOMATION_DATA_DIR}/from_product"
+    FROM_PRODUCT_TEMP_DIR="${FROM_PRODUCT_BASE_DIR}/temp"
+    mkdir -p "${FROM_PRODUCT_TEMP_DIR}"
     ${AUTOMATION_DIR}/manageRepo.sh -c -l "from product config" \
         -n "${FROM_PRODUCT_CONFIG_REPO}" -v "${FROM_PRODUCT_GIT_PROVIDER}" \
-        -d "${FROM_PRODUCT_DIR}" -b "${ACCEPTANCE_TAG}"
+        -d "${FROM_PRODUCT_TEMP_DIR}" -b "${ACCEPTANCE_TAG}"
     RESULT=$? && [[ "${RESULT}" -ne 0 ]] && exit
 
-    FROM_SETTINGS_DIR=${FROM_PRODUCT_DIR}/appsettings/${FROM_SEGMENT}
+    # Allow for composite repos
+    if [[ -n $(findDir "${FROM_PRODUCT_TEMP_DIR}" "${PRODUCT}") ]]; then
+        FROM_PRODUCT_ROOT_DIR="${FROM_PRODUCT_BASE_DIR}/config"
+    else
+        FROM_PRODUCT_ROOT_DIR="${FROM_PRODUCT_BASE_DIR}/${PRODUCT}"
+    fi
+    mv "${FROM_PRODUCT_TEMP_DIR}" "${FROM_PRODUCT_ROOT_DIR}"
+
+    # Find the product appsettings directory
+    FROM_SETTINGS_DIR="$(findGen3ProductDir "${FROM_PRODUCT_BASE_DIR}" "${PRODUCT}")/appsettings/${SEGMENT}"
             
     # Pull in the current build references in lower segment
     ${AUTOMATION_DIR}/manageBuildReferences.sh -f -g ${FROM_SETTINGS_DIR}

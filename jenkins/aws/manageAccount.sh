466a5f4fd01b06b48241f202bd30e6195b7b72a5
Michael Leditschke <mike@ammd.com.au>
2017-11-10 15:53:53 +1100

    Update manageAccount script

--- jenkins/aws/manageAccount.sh
+++ jenkins/aws/manageAccount.sh
@@ -4,47 +4,49 @@
 trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
 . "${GENERATION_DIR}/common.sh"
 
-findGen3Dirs "${AUTOMATION_DATA_DIR}/${ACCOUNT}"
+# Remember if anything was processed
+SAVE_REQUIRED="false"
 
-# Create the account level buckets if required
-if [[ "${CREATE_ACCOUNT_BUCKETS}" == "true" ]]; then
-    cd ${ACCOUNT_DIR}
-    ${GENERATION_DIR}/createAccountTemplate.sh -a ${ACCOUNT}
-    RESULT=$?
-    [[ "${RESULT}" -ne 0 ]] &&
-        fatal "Generation of the account level template for the ${ACCOUNT} account failed"
+# Determine the units to process
+LEVEL="account"
+IFS="${DEPLOYMENT_UNIT_SEPARATORS}" read -ra UNITS <<< "${ACCOUNT_UNITS_LIST}"
 
-    # Create the stack
-    ${GENERATION_DIR}/createStack.sh -t account
-	RESULT=$?
-    [[ "${RESULT}" -ne 0 ]] &&
-        fatal "Creation of the account level stack for the ${ACCOUNT} account failed"
-        
-    # Update the infrastructure repo to capture any stack changes
-    cd ${ACCOUNT_INFRASTRUCTURE_DIR}
+# Reverse the order if we are deleting
+[[ "${DEPLOYMENT_MODE}" == "${DEPLOYMENT_MODE_STOP}" ]] && reverseArray UNITS
 
-    # Ensure git knows who we are
-    git config user.name  "${BUILD_USER}"
-    git config user.email "${BUILD_USER_EMAIL}"
+for CURRENT_DEPLOYMENT_UNIT in "${UNITS[@]}"; do
+  
+  # Say what we are doing
+  info "Processing \"${LEVEL}\" level, \"${CURRENT_DEPLOYMENT_UNIT}\" unit ...\n"
 
-    # Record changes
-    git add *
-    git commit -m "Stack changes as a result of creating the ${ACCOUNT} account stack"
-    git push origin master
-	RESULT=$?
-    [[ "${RESULT}" -ne 0 ]] &&
-        fatal "Unable to save the changes resulting from creating the ${ACCOUNT} account stack"
-fi
+  # Generate the template if required
+  if [[ ("${DEPLOYMENT_MODE}" == "${DEPLOYMENT_MODE_UPDATE}") ]]; then
+    ${AUTOMATION_DIR}/createTemplates.sh -u "${CURRENT_DEPLOYMENT_UNIT}" -l "${LEVEL}" -c "${INFRASTRUCTURE_TAG}"
+    RESULT=$? && [[ "${RESULT}" -ne 0 ]] && exit 
+  fi
+
+  ${AUTOMATION_DIR}/manageStacks.sh -u "${CURRENT_DEPLOYMENT_UNIT}" -l "${LEVEL}"
+  RESULT=$? && [[ "${RESULT}" -ne 0 ]] && exit
+
+  SAVE_REQUIRED="true"
+done
 
 # Update the code and credentials buckets if required
 if [[ "${SYNC_ACCOUNT_BUCKETS}" == "true" ]]; then
-    cd ${AUTOMATION_DATA_DIR}/${ACCOUNT}
+    cd ${ACCOUNT_DIR}
     ${GENERATION_DIR}/syncAccountBuckets.sh -a ${ACCOUNT}
 fi
 
-# All good
-RESULT=0
+# All good - save the result
+if [[ "${SAVE_REQUIRED}" == "true" ]]; then
+  info "Saving changes under tag \"${INFRASTRUCTURE_TAG}\" ..."
 
+  save_product_infrastructure \
+    "${DETAIL_MESSAGE}, level=segment, units=cmk" \
+    "${PRODUCT_INFRASTRUCTURE_REFERENCE}" \
+    "${INFRASTRUCTURE_TAG}"
+  RESULT=$?
+fi
 
 
 

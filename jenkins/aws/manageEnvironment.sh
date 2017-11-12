b98db57e7d3a4ffec0ede9fdbde7a92bed8ffbcc
Michael Leditschke <mike@ammd.com.au>
2017-11-12 20:36:26 +1100

    manageUnits.sh now doesn't manage saving results

--- jenkins/aws/manageEnvironment.sh
+++ jenkins/aws/manageEnvironment.sh
@@ -1,56 +1,20 @@
 #!/bin/bash
 
 [[ -n "${AUTOMATION_DEBUG}" ]] && set ${AUTOMATION_DEBUG}
-trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
+trap 'exit 1' SIGHUP SIGINT SIGTERM
 . "${AUTOMATION_BASE_DIR}/common.sh"
 
-# Remember if anything was processed
-SAVE_REQUIRED="false"
+function main() {
+  TAG="env${AUTOMATION_JOB_IDENTIFIER}-${PRODUCT}-${SEGMENT}"
 
-# Configuration reference tag
-INFRASTRUCTURE_TAG="env${AUTOMATION_JOB_IDENTIFIER}-${SEGMENT}"
+  ${AUTOMATION_DIR}/manageUnits -r "${TAG}" || return $?
 
-# Process each template level
-IFS="${DEPLOYMENT_UNIT_SEPARATORS}" read -ra LEVELS_REQUIRED <<< "${LEVELS}"
+  # All ok so tag the config repo
+  save_product_config "${DETAIL_MESSAGE}" "${PRODUCT_CONFIG_REFERENCE}" "${TAG}" || return $?
+  
+  # Commit the generated application templates
+  save_product_infrastructure "${DETAIL_MESSAGE}" "${PRODUCT_INFRASTRUCTURE_REFERENCE}" "${TAG}" || return $?
+}
 
-# Reverse the order if we are deleting
-[[ "${DEPLOYMENT_MODE}" == "${DEPLOYMENT_MODE_STOP}" ]] && reverseArray LEVELS_REQUIRED
-
-for LEVEL in "${LEVELS_REQUIRED[@]}"; do
-  UNITS_LIST="${LEVEL^^}_UNITS_LIST"
-  IFS="${DEPLOYMENT_UNIT_SEPARATORS}" read -ra UNITS <<< "${!UNITS_LIST}"
-
-  # Reverse the order if we are deleting
-  [[ "${DEPLOYMENT_MODE}" == "${DEPLOYMENT_MODE_STOP}" ]] && reverseArray UNITS
-
-  # Manage the stacks individually in case of failure and becuase one can depend on the 
-  # output of the previous one
-  for CURRENT_DEPLOYMENT_UNIT in "${UNITS[@]}"; do
-    
-    # Say what we are doing
-    info "Processing \"${LEVEL}\" level, \"${CURRENT_DEPLOYMENT_UNIT}\" unit ...\n"
-
-    # Generate the template if required
-    if [[ ("${DEPLOYMENT_MODE}" == "${DEPLOYMENT_MODE_UPDATE}") ]]; then
-      ${AUTOMATION_DIR}/createTemplates.sh -u "${CURRENT_DEPLOYMENT_UNIT}" -l "${LEVEL}" -c "${INFRASTRUCTURE_TAG}"
-      RESULT=$? && [[ "${RESULT}" -ne 0 ]] && exit 
-    fi
-
-    ${AUTOMATION_DIR}/manageStacks.sh -u "${CURRENT_DEPLOYMENT_UNIT}" -l "${LEVEL}"
-    RESULT=$? && [[ "${RESULT}" -ne 0 ]] && exit
-
-    SAVE_REQUIRED="true"
-  done
-done
-
-# All good - save the result
-if [[ "${SAVE_REQUIRED}" == "true" ]]; then
-  info "Saving changes under tag \"${INFRASTRUCTURE_TAG}\" ..."
-
-  save_product_infrastructure \
-    "${DETAIL_MESSAGE}, level=segment, units=cmk" \
-    "${PRODUCT_INFRASTRUCTURE_REFERENCE}" \
-    "${INFRASTRUCTURE_TAG}"
-  RESULT=$?
-fi
+main "$@"
 
--- jenkins/aws/manageUnits.sh
+++ jenkins/aws/manageUnits.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 [[ -n "${AUTOMATION_DEBUG}" ]] && set ${AUTOMATION_DEBUG}
-trap 'exit $?' EXIT SIGHUP SIGINT SIGTERM
+trap 'exit 1' SIGHUP SIGINT SIGTERM
 . "${AUTOMATION_BASE_DIR}/common.sh"
-
-# Defaults
-LEVELS_DEFAULT="application"
 
 function usage() {
     cat <<EOF
 
 Manage one or more deployment units in one or more levels
 
-Usage: $(basename $0) -s DEPLOYMENT_UNIT_LIST -l LEVEL_LIST -m DEPLOYMENT_MODE
+Usage: $(basename $0) -l LEVELS_LIST -r REFERENCE -m DEPLOYMENT_MODE
+                      -c ACCOUNT_UNITS_LIST
+                      -p PRODUCT_UNITS_LIST
+                      -a APPLICATION_UNITS_LIST
+                      -s SOLUTION_UNITS_LIST
+                      -f SEGMENT_UNITS_LIST
+                      -u MULTIPLE_UNITS_LIST
+                      -t UNITS_TAG
 
 where
 
@@ -20,28 +24,28 @@ where
 (o) -c ACCOUNT_UNITS_LIST     is the list of account level units to process
 (o) -g SEGMENT_UNITS_LIST     is the list of segment level units to process
     -h                        shows this text
-(m) -l LEVELS                 is the list of levels to consider
-(m) -m DEPLOYMENT_MODE        is the deployment mode
+(m) -l LEVELS_LIST            is the list of levels to consider
+(o) -m DEPLOYMENT_MODE        is the deployment mode if stacks are to be managed
 (o) -p PRODUCT_UNITS_LIST     is the list of product level units to process
+(o) -r REFERENCE              reference to use when preparing templates
 (o) -s SOLUTION_UNITS_LIST    is the list of solution level units to process
-(o) -t INFRASTRUCTURE_TAG     is the tag to use when saving the result
 (o) -u MULTIPLE_UNITS_LIST    is the list of multi-level units to process
 
 (m) mandatory, (o) optional, (d) deprecated
 
 DEFAULTS:
 
-DEPLOYMENT_MODE = "${DEPLOYMENT_MODE_DEFAULT}"
-LEVELS = "${LEVELS_DEFAULT}"
-
 NOTES:
+
+1. Presence of -r triggers generation of templates
+2. Presence of -m triggers management of stacks
 
 EOF
 }
 
 function options() {
     # Parse options
-    while getopts ":a:c:g:hl:m:p:s:u:" option; do
+    while getopts ":a:c:g:hl:m:p:r:s:u:" option; do
         case $option in
             a) APPLICATION_UNITS_LIST="${OPTARG}" ;;
             c) ACCOUNT_UNITS_LIST="${OPTARG}" ;;
@@ -49,24 +53,15 @@ function options() {
             h) usage; return 1 ;;
             l) LEVELS_LIST="${OPTARG}" ;;
             m) DEPLOYMENT_MODE="${OPTARG}" ;;
-            p) PRODUCT_UNIT_LIST="${OPTARG}" ;;
-            s) SOLUTION_UNIT_LIST="${OPTARG}" ;;
-            t) INFRASTRUCTURE_TAG="${OPTARG}" ;;
-            u) MULTIPLE_UNIT_LIST="${OPTARG}" ;;
+            p) PRODUCT_UNITS_LIST="${OPTARG}" ;;
+            r) REFERENCE="${OPTARG}" ;;
+            s) SOLUTION_UNITS_LIST="${OPTARG}" ;;
+            u) MULTIPLE_UNITS_LIST="${OPTARG}" ;;
             \?) fatalOption; return 1 ;;
             :) fatalOptionArgument; return 1 ;;
          esac
     done
-    
-    # Apply defaults
-    export DEPLOYMENT_MODE="${DEPLOYMENT_MODE:-${DEPLOYMENT_MODE_DEFAULT}}"
-    export LEVELS_LIST="${LEVELS_LIST:-${LEVEL_DEFAULT}}"
-    export INFRASTRUCTURE_TAG="${INFRASTRUCTURE_TAG:-env${AUTOMATION_JOB_IDENTIFIER}-${SEGMENT}}"
-    
-    # Ensure mandatory arguments have been provided
-    [[ (-z "${DEPLOYMENT_MODE}") || (-z "${LEVEL}") ]] && 
-        fatalMandatory && return 1
-    
+
     return 0
 }
 
@@ -74,9 +69,6 @@ function main() {
 
   options "$@" || return $?
 
-  # Remember if anything was processed
-  save_required="false"
-  
   # Process each template level
   arrayfromList levels_required "${LEVELS_LIST}"
   
@@ -101,43 +93,31 @@ function main() {
     # Reverse the order if we are deleting
     [[ "${DEPLOYMENT_MODE}" == "${DEPLOYMENT_MODE_STOP}" ]] && reverseArray units
     
-    # Manage the units individually in case of failure and because one can depend on the 
-    # output of the previous one
+    # Manage the units individually as one can depend on the output of previous ones
     for unit in "${units[@]}"; do
     
       # Say what we are doing
       info "Processing \"${level}\" level, \"${unit}\" unit ...\n"
       
       # Generate the template if required
-      if [[ ("${DEPLOYMENT_MODE}" == "${DEPLOYMENT_MODE_UPDATE}") ]]; then
-        ${GENERATION_DIR}/createTemplate.sh -u "${unit}" -l "${level}" -c "${INFRASTRUCTURE_TAG}" || return $?
+      if [[ -n "${REFERENCE}" ]]; then
+        ${GENERATION_DIR}/createTemplate.sh -u "${unit}" -l "${level}" -c "${REFERENCE}" || return $?
       fi
-      
-      ${AUTOMATION_DIR}/manageStacks.sh -u "${unit}" -l "${level}" || return $?
 
-      if [[ "${DEPLOYMENT_MODE}" != "${DEPLOYMENT_MODE_UPDATE}" ]]; then
-          ${GENERATION_DIR}/manageStack.sh -u ${unit} -d ||
-              { exit_status=$?; fatal "Deletion of the ${level} level stack for the ${unit} deployment unit failed"; return "${exit_status}"; }
+      # Manage the stack if required
+      if [[ -n "${DEPLOYMENT_MODE}" ]]; then
+        if [[ "${DEPLOYMENT_MODE}" != "${DEPLOYMENT_MODE_UPDATE}" ]]; then
+            ${GENERATION_DIR}/manageStack.sh -u ${unit} -d ||
+                { exit_status=$?; fatal "Deletion of the ${level} level stack for the ${unit} deployment unit failed"; return "${exit_status}"; }
+        fi
+        if [[ "${DEPLOYMENT_MODE}" != "${DEPLOYMENT_MODE_STOP}"   ]]; then
+            ${GENERATION_DIR}/manageStack.sh -u ${unit} ||
+                { exit_status=$?; fatal "Create/update of the ${level} level stack for the ${unit} deployment unit failed"; return "${exit_status}"; }
+        fi
       fi
-      if [[ "${DEPLOYMENT_MODE}" != "${DEPLOYMENT_MODE_STOP}"   ]]; then
-          ${GENERATION_DIR}/manageStack.sh -u ${unit} ||
-              { exit_status=$?; fatal "Create/update of the ${level} level stack for the ${unit} deployment unit failed"; return "${exit_status}"; }
-      fi
-      save_required="true"
     done
   done
   
-  # All good - save the result
-  if [[ "${save_required}" == "true" ]]; then
-    info "Saving changes under tag \"${INFRASTRUCTURE_TAG}\" ..."
-    
-    save_product_infrastructure \
-    "${DETAIL_MESSAGE}, level=segment, units=cmk" \
-    "${PRODUCT_INFRASTRUCTURE_REFERENCE}" \
-    "${INFRASTRUCTURE_TAG}"
-    RESULT=$?
-  fi
-
   return 0
 }
 

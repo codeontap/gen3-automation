b0ab1214d6816aa42b37476ab5b0a7d724969691
Michael Leditschke <mike@ammd.com.au>
2017-11-12 21:06:13 +1100

    Use manageUnits for all template/stack control
    createTemplates.sh and manageStacks.sh are now in a single script as the
    logic is similar, and we are moving to running all units as part of a
    deploy.

--- jenkins/aws/prepareRelease.sh
+++ jenkins/aws/prepareRelease.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
 [[ -n "${AUTOMATION_DEBUG}" ]] && set ${AUTOMATION_DEBUG}
-trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
+trap 'exit 1' EXIT SIGHUP SIGINT SIGTERM
 . "${AUTOMATION_BASE_DIR}/common.sh"
 
-# Update build references
-${AUTOMATION_DIR}/manageBuildReferences.sh -u
-RESULT=$?
-[[ ${RESULT} -ne 0 ]] && exit
+function main() {
+  # Update build references
+  ${AUTOMATION_DIR}/manageBuildReferences.sh -u || return $?
+  
+  # Create the templates
+  ${AUTOMATION_DIR}/manageUnits.sh -l "application" -a "${DEPLOYMENT_UNITS}" -r "${RELEASE_TAG}" || return $?
+  
+  # All ok so tag the config repo
+  save_product_config "${DETAIL_MESSAGE}" "${PRODUCT_CONFIG_REFERENCE}" "${RELEASE_TAG}" || return $?
+  
+  # Commit the generated application templates
+  save_product_infrastructure "${DETAIL_MESSAGE}" "${PRODUCT_INFRASTRUCTURE_REFERENCE}" "${RELEASE_TAG}" || return $?
 
-# Create the templates
-${AUTOMATION_DIR}/createTemplates.sh -l application -c "${RELEASE_TAG}"
-RESULT=$? && [[ ${RESULT} -ne 0 ]] && exit
+  # Record key parameters for downstream jobs
+  save_context_property RELEASE_IDENTIFIER "${AUTOMATION_RELEASE_IDENTIFIER}"
 
-# All ok so tag the config repo
-save_product_config "${DETAIL_MESSAGE}" "${PRODUCT_CONFIG_REFERENCE}" "${RELEASE_TAG}"
-RESULT=$? && [[ ${RESULT} -ne 0 ]] && exit
+  return 0
+}
 
-# Commit the generated application templates
-save_product_infrastructure "${DETAIL_MESSAGE}" "${PRODUCT_INFRASTRUCTURE_REFERENCE}" "${RELEASE_TAG}"
-RESULT=$? && [[ ${RESULT} -ne 0 ]] && exit
-
-# Record key parameters for downstream jobs
-save_context_property RELEASE_IDENTIFIER "${AUTOMATION_RELEASE_IDENTIFIER}"
-
-# All good
-RESULT=0
+main "$@"

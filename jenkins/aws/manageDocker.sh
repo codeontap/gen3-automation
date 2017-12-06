8c0f07e02a0d9a60d6139c473a3c67d8277cd004
Michael Leditschke <mike@ammd.com.au>
2017-12-06 21:47:36 +1100

    Use the build dir as the docker build context

--- jenkins/aws/manageDocker.sh
+++ jenkins/aws/manageDocker.sh
@@ -272,15 +272,9 @@ case ${DOCKER_OPERATION} in
             DOCKERFILE="${AUTOMATION_BUILD_DEVOPS_DIR}/docker/Dockerfile"
         fi
 
-        # Prepare the build directory - docker file must be in build context
-        DOCKER_BUILD_DIR="$(getTempDir "docker_XXX")"
-        cp -p  "${DOCKERFILE}" "${DOCKER_BUILD_DIR}"
-        cp -rp "${AUTOMATION_BUILD_SRC_DIR}" "${DOCKER_BUILD_DIR}"
-
         # Perform the build
-        docker build -t "${FULL_DOCKER_IMAGE}" "${DOCKER_BUILD_DIR}"
+        docker build -t "${FULL_DOCKER_IMAGE}" -f "${DOCKERFILE}" "${AUTOMATION_BUILD_DIR}"
         RESULT=$?
-        rm -rf "${DOCKER_BUILD_DIR}"
 
         [[ $RESULT -ne 0 ]] && fatal "Cannot build image ${DOCKER_IMAGE}" && exit
 

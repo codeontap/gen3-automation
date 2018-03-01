2237ec4423670b791a1634b1838a6122744d4595
Michael Foley <roleyfoley@gmail.com>
2018-03-01 14:59:05 +1100

    Add Content Node build and image management (#44)
    * Remove junk cleanup on zip
    
    * Add Content node content build tasks
    
    * Update build location to content

---
+++ jenkins/aws/manageContentNode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+[[ -n "${AUTOMATION_DEBUG}" ]] && set ${AUTOMATION_DEBUG}
+trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
+
+# Note that filename can still overridden via provided parameters
+${AUTOMATION_DIR}/manageS3Registry.sh -y "contentnode" -f "contentnode.zip" "$@"
+RESULT=$?

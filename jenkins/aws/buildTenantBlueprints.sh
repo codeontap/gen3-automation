92df7b31be783dafb4b1fa1bc0b7a6f1277748f6
roleyfoley <roleyfoley@gmail.com>
2018-03-21 12:53:59 +1100

    Documentation and refactor for destination file

--- jenkins/aws/buildTenantBlueprints.sh
+++ jenkins/aws/buildTenantBlueprints.sh
@@ -11,11 +11,14 @@ function main() {
     # Make sure we are in the build source directory
     cd ${AUTOMATION_BUILD_SRC_DIR}
 
+    # By default store consolidated blueprints in the tenant infrastructure directory
     BLUEPRINT_CONSOLIDATION_PREFIX="${INFRADOCS_DIR:-${TENANT_INFRASTRUCTURE_DIR}}"
     BLUEPRINT_CONSOLIDATION_REPO="${INFRADOCS_REPO:-${ACCOUNT_INFRASTRUCTURE_REPO}}"
 
     BLUEPRINT_CONSOLIDATION_TEMP="${AUTOMATION_BUILD_SRC_DIR}/blueprints"
     BLUEPRINT_CONSOLIDATION_DIR="${BLUEPRINT_CONSOLIDATION_TEMP}/${BLUEPRINT_CONSOLIDATION_PREFIX}"
+
+    BLUEPRINT_DESTINATION_FILE="${BLUEPRINT_CONSOLIDATION_DIR}/cot/blueprints/${TENANT}/${PRODUCT}/${ENVIRONMENT}/${SOLUTION}/${SEGMENT}/blueprint.json"
 
     ${AUTOMATION_DIR}/manageRepo.sh -c -l "blueprint consolidation" \
     -n "${BLUEPRINT_CONSOLIDATION_REPO}" -v "${ACCOUNT_GIT_PROVIDER}" \
@@ -24,13 +27,13 @@ function main() {
     if [[ -f "${AUTOMATION_BUILD_SRC_DIR}/blueprint.json"]]; then 
 
         echo "Adding Blueprint to Tenant Infrastructure..."
-        cp "${AUTOMATION_BUILD_SRC_DIR}/blueprint.json" "${BLUEPRINT_CONSOLIDATION_DIR}/cot/blueprints/${TENANT}/${PRODUCT}/${ENVIRONMENT}/${SOLUTION}/${SEGMENT}/blueprint.json"
+        cp "${AUTOMATION_BUILD_SRC_DIR}/blueprint.json" "${BLUEPRINT_DESTINATION_FILE}"
     
     else
 
-        if [[ -f "${BLUEPRINT_CONSOLIDATION_DIR}/cot/blueprints/${TENANT}/${PRODUCT}/${ENVIRONMENT}/${SOLUTION}/${SEGMENT}/blueprint.json" ]]; then 
+        if [[ -f "${BLUEPRINT_DESTINATION_FILE}" ]]; then 
             echo "Removing Blueprint from Tenant Infrastructure..."
-            rm "${BLUEPRINT_CONSOLIDATION_DIR}/cot/blueprints/${TENANT}/${PRODUCT}/${ENVIRONMENT}/${SOLUTION}/${SEGMENT}/blueprint.json"
+            rm "${BLUEPRINT_DESTINATION_FILE}"
         fi 
     fi
 

cc1b1f79da5e09658817a4b4bf05743b7279a5e2
roleyfoley <roleyfoley@gmail.com>
2018-03-26 16:18:56 +1100

    Remove apidoc test as it is always generated

--- jenkins/aws/manageSwagger.sh
+++ jenkins/aws/manageSwagger.sh
@@ -5,10 +5,7 @@ trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
 
 # Note that filename can still overridden via provided parameters
 ${AUTOMATION_DIR}/manageS3Registry.sh -x -y "swagger" -f "swagger.zip" "$@"
-
-if [[ -f "apidoc.zip" ]]; then 
-    ${AUTOMATION_DIR}/manageS3Registry.sh -x -y "swagger" -f "apidoc.zip" "$@"
-fi
+${AUTOMATION_DIR}/manageS3Registry.sh -x -y "swagger" -f "apidoc.zip" "$@"
 
 RESULT=$?
 

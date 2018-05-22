d922689eb6c8e696aba905fd2fd3951455365d97
roleyfoley <roleyfoley@gmail.com>
2018-05-22 11:04:00 +1000

    Add a basic build of static content for SPA

---
+++ jenkins/aws/buildSPAStatic.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# - Basic build for an SPA that is static content
+
+[[ -n "${AUTOMATION_DEBUG}" ]] && set ${AUTOMATION_DEBUG}
+trap '[[ (-z "${AUTOMATION_DEBUG}") ; exit 1' SIGHUP SIGINT SIGTERM
+. "${AUTOMATION_BASE_DIR}/common.sh"
+
+function main() {
+  # Make sure we are in the build source directory
+  cd ${AUTOMATION_BUILD_SRC_DIR}
+  
+  # packge for content node
+  if [[ -d "${AUTOMATION_BUILD_SRC_DIR}" ]]; then
+    mkdir -p "${AUTOMATION_BUILD_SRC_DIR}/dist"
+    
+    cd "${AUTOMATION_BUILD_SRC_DIR}"
+    zip -r "${AUTOMATION_BUILD_SRC_DIR}/dist/spa.zip" * 
+
+  fi
+
+  # All good
+  return 0
+}
+
+main "$@"
+

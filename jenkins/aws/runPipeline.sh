b8115599ca52bb72d4448cb1dbd8685a921dc9a7
roleyfoley <roleyfoley@gmail.com>
2018-12-18 16:17:58 +1100

    Add pipeline run script

---
+++ jenkins/aws/runPipeline.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+[[ -n "${AUTOMATION_DEBUG}" ]] && set ${AUTOMATION_DEBUG}
+trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
+. "${AUTOMATION_BASE_DIR}/common.sh"
+
+cd "${SEGMENT_SOLUTIONS_DIR}"
+
+# run the required tasks
+${GENERATION_DIR}/runPipeline.sh -t "${PIPELINE_TIER}" -i "${PIPLINE_COMPONENT}" -x "${PIPELINE_INSTANCE}" -y "${PIPELINE_VERSION}" -c "${PIPELINE_CHECKONLY}"
+RESULT=$?
+[[ ${RESULT} -ne 0 ]] &&
+        fatal "Running of pipepline ${PIPELINE_COMPONENT} - ${PIPELINE-INSTANCE} - ${PIPELINE_VERSION} failed" && exit $RESULT
+
+# All good
+RESULT=0

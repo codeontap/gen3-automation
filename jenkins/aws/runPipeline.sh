7f3344eef0fb6208c4bff1ff215a397296ac04e1
roleyfoley <roleyfoley@gmail.com>
2019-01-10 15:17:18 +1100

    Typo fix for automated pipeline activation

--- jenkins/aws/runPipeline.sh
+++ jenkins/aws/runPipeline.sh
@@ -7,7 +7,7 @@ trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
 cd "${SEGMENT_SOLUTIONS_DIR}"
 
 # run the required tasks
-${GENERATION_DIR}/runPipeline.sh -t "${PIPELINE_TIER}" -i "${PIPLINE_COMPONENT}" -x "${PIPELINE_INSTANCE}" -y "${PIPELINE_VERSION}" -c "${PIPELINE_CHECKONLY}"
+${GENERATION_DIR}/runPipeline.sh -t "${PIPELINE_TIER}" -i "${PIPELINE_COMPONENT}" -x "${PIPELINE_INSTANCE}" -y "${PIPELINE_VERSION}" -c "${PIPELINE_CHECKONLY}"
 RESULT=$?
 [[ ${RESULT} -ne 0 ]] &&
         fatal "Running of pipepline ${PIPELINE_COMPONENT} - ${PIPELINE-INSTANCE} - ${PIPELINE_VERSION} failed" && exit $RESULT

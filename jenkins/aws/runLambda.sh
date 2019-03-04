82e13189ea3737ef0b2baeaddffe1ea1889046a0
roleyfoley <roleyfoley@gmail.com>
2019-03-04 14:23:54 +1100

    Add lambda function Id as paramter

--- jenkins/aws/runLambda.sh
+++ jenkins/aws/runLambda.sh
@@ -17,7 +17,7 @@ if [[ -n "${LAMBDA_INPUT_PAYLOAD}" ]]; then
 fi 
 
 # run the required tasks
-${GENERATION_DIR}/runLambda.sh -u "${LAMBDA_DEPLOYMENT_UNIT}" "${ARGS}"
+${GENERATION_DIR}/runLambda.sh -u "${LAMBDA_DEPLOYMENT_UNIT}" -f "${LAMBDA_FUNCTION_ID}" "${ARGS}"
 RESULT=$?
 [[ ${RESULT} -ne 0 ]] &&
         fatal "Running of lambda ${LAMBDA_DEPLOYMENT_UNIT} failed" && exit $RESULT

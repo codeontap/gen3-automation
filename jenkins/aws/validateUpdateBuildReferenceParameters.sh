12b95a176e7905c3b802a386941974265a4fa642
roleyfoley <roleyfoley@gmail.com>
2019-05-22 00:15:16 +1000

    fix up syntax for validation

--- jenkins/aws/validateUpdateBuildReferenceParameters.sh
+++ jenkins/aws/validateUpdateBuildReferenceParameters.sh
@@ -4,7 +4,7 @@
 trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
 . "${AUTOMATION_BASE_DIR}/common.sh"
 
-[[ -z ${CODE_COMMIT_LIST} && -z ${CODE_TAG_LIST} ]] &&
+[[ ( -z "${CODE_COMMIT_LIST}" ) && ( -z "${CODE_TAG_LIST}" ) ]] &&
     fatal "This job requires a GIT_COMMIT or a DEPLOYMENT_UNIT! value" && exit
 
 # Ensure at least one deployment unit has been provided

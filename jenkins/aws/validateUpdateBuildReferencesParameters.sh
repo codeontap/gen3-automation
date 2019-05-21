a3ba4c3bb51d040f3fa24fd468b85810fa1fd576
roleyfoley <roleyfoley@gmail.com>
2019-05-22 00:19:10 +1000

    update both validations

--- jenkins/aws/validateUpdateBuildReferencesParameters.sh
+++ jenkins/aws/validateUpdateBuildReferencesParameters.sh
@@ -4,8 +4,8 @@
 trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
 . "${AUTOMATION_BASE_DIR}/common.sh"
 
-[[ -z ${GIT_COMMIT} ]] &&
-    fatal "This job requires a GIT_COMMIT value" && exit
+[[ ( -z "${CODE_COMMIT_LIST}" ) && ( -z "${CODE_TAG_LIST}" ) ]] &&
+    fatal "This job requires a GIT_COMMIT or a DEPLOYMENT_UNIT! value" && exit
 
 # Ensure at least one deployment unit has been provided
 [[ -z "${DEPLOYMENT_UNIT_LIST}" ]] &&
@@ -14,7 +14,6 @@ trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
 # Ensure at least one deployment unit has been provided
 [[ ( -z "${IMAGE_FORMAT}" ) && ( -z "${IMAGE_FORMATS}" ) ]] &&
     fatal "Job requires the image format used to package the build" && exit
-
 
 # All good
 RESULT=0

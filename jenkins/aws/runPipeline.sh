9f67413dd980a4129157c7a2755807dac13c23f7
roleyfoley <roleyfoley@gmail.com>
2019-08-21 15:16:02 +1000

    <fix> Align pipeline automation with pipeline task

--- jenkins/aws/runPipeline.sh
+++ jenkins/aws/runPipeline.sh
@@ -7,10 +7,22 @@ trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
 cd "${SEGMENT_SOLUTIONS_DIR}"
 
 # run the required tasks
-${GENERATION_DIR}/runPipeline.sh -t "${PIPELINE_TIER}" -i "${PIPELINE_COMPONENT}" -x "${PIPELINE_INSTANCE}" -y "${PIPELINE_VERSION}" -c "${PIPELINE_CHECKONLY}"
+
+EXTRA_ARGS=""
+if [[ -n "${PIPELINE_STATUS_ONLY}" ]]; then
+        EXTRA_ARGS="${EXTRA_ARGS} -s"
+fi
+
+if [[ -n "${PIPELINE_ALLOW_CONCURRENT}" ]]; then 
+        EXTRA_ARGS="${EXTRA_ARGS} -c"
+fi
+
+${GENERATION_DIR}/runPipeline.sh -t "${PIPELINE_TIER}" -i "${PIPELINE_COMPONENT}" -x "${PIPELINE_INSTANCE}" -y "${PIPELINE_VERSION}" $EXTRA_ARGS
 RESULT=$?
-[[ ${RESULT} -ne 0 ]] &&
-        fatal "Running of pipepline ${PIPELINE_COMPONENT} - ${PIPELINE-INSTANCE} - ${PIPELINE_VERSION} failed" && exit $RESULT
+
+if [[ ${RESULT} -ne 0 ]]; then 
+        fatal "Running of pipepline ${PIPELINE_COMPONENT} - ${PIPELINE_INSTANCE} - ${PIPELINE_VERSION} failed" 
+fi
 
 # All good
-RESULT=0
+RESULT="${RESULT:-0}"
